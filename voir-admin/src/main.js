23b3bf25ca62427346f782aaab68b3d0be475305
Huang JX <hjxlog@gmail.com>
2022-03-22 08:50:56 +0800

    update

--- voir-admin/src/main.js
+++ voir-admin/src/main.js
@@ -6,9 +6,12 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import './assets/css/global.css'
 import * as ELIcons from '@element-plus/icons-vue'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
 
 const app = createApp(App);
-app.use(store).use(router).use(ElementPlus)
+app.use(store).use(router).use(ElementPlus, {
+    locale: zhCn,
+})
 for(const iconName in ELIcons){
     app.component(iconName,ELIcons[iconName])
 }
--- voir-admin/src/views/HomeView.vue
+++ voir-admin/src/views/HomeView.vue
@@ -2,7 +2,7 @@
   <!--  搜索框按钮区域-->
   <el-row style="padding-left: 10px;margin-top: 10px;margin-bottom: -20px;">
     <el-col :span="3" style="margin-right: -30px;">
-      <el-button type="primary">新增博客</el-button>
+      <el-button type="primary" @click="add">新增博客</el-button>
     </el-col>
     <el-col :span="21">
       <el-form :inline="true" class="demo-form-inline">
@@ -19,9 +19,9 @@
     <el-table :data="tableData"
               stripe border height="75vh"
               style="width: 100%">
-      <el-table-column sortable prop="date" label="Date"/>
-      <el-table-column prop="name" label="Name"/>
-      <el-table-column prop="address" label="Address"/>
+      <el-table-column sortable prop="id" label="ID"/>
+      <el-table-column prop="title" label="标题"/>
+      <el-table-column prop="content" label="内容"/>
       <el-table-column fixed="right" label="Operations" width="120">
         <template #default>
           <el-button type="text" @click="handleClick">编辑</el-button>
@@ -51,116 +51,64 @@
     </div>
   </div>
 
+  <el-dialog
+      v-model="dialogVisible"
+      title="Tips"
+      width="30%"
+  >
+    <el-form :model="form" label-width="120px">
+      <el-form-item label="博客标题">
+        <el-input v-model="form.title"/>
+      </el-form-item>
+      <el-form-item label="博客分类">
+        <el-select v-model="form.categoryId" placeholder="请选择分类">
+          <el-option label="Java" value="Java"/>
+          <el-option label="Spring" value="Spring"/>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="博客内容">
+        <el-input v-model="form.content" type="textarea"/>
+      </el-form-item>
+    </el-form>
+    <template #footer>
+      <span class="dialog-footer">
+        <el-button @click="dialogVisible = false">取消</el-button>
+        <el-button type="primary" @click="save">确定</el-button>
+      </span>
+    </template>
+  </el-dialog>
+
 </template>
 
 <script>
+import request from "@/utils/request";
+
 export default {
   name: "HomeView",
   data() {
     return {
+      form: {},
+      dialogVisible: false,
       tableData: [
         {
-          date: '2016-05-03',
-          name: 'Tom',
-          address: 'No. 189, Grove St, Los Angeles',
-        },
-        {
-          date: '2016-05-02',
-          name: 'Tom',
-          address: 'No. 189, Grove St, Los Angeles',
-        },
-        {
-          date: '2016-05-04',
-          name: 'Tom',
-          address: 'No. 189, Grove St, Los Angeles',
-        },
-        {
-          date: '2016-05-01',
-          name: 'Tom',
-          address: 'No. 189, Grove St, Los Angeles',
-        },
-
-        {
-          date: '2016-05-03',
-          name: 'Tom',
-          address: 'No. 189, Grove St, Los Angeles',
-        },
-        {
-          date: '2016-05-02',
-          name: 'Tom',
-          address: 'No. 189, Grove St, Los Angeles',
-        },
-        {
-          date: '2016-05-04',
-          name: 'Tom',
-          address: 'No. 189, Grove St, Los Angeles',
-        },
-        {
-          date: '2016-05-01',
-          name: 'Tom',
-          address: 'No. 189, Grove St, Los Angeles',
-        },
-        {
-          date: '2016-05-03',
-          name: 'Tom',
-          address: 'No. 189, Grove St, Los Angeles',
-        },
-        {
-          date: '2016-05-02',
-          name: 'Tom',
-          address: 'No. 189, Grove St, Los Angeles',
-        },
-        {
-          date: '2016-05-04',
-          name: 'Tom',
-          address: 'No. 189, Grove St, Los Angeles',
-        },
-        {
-          date: '2016-05-01',
-          name: 'Tom',
-          address: 'No. 189, Grove St, Los Angeles',
-        },
-        {
-          date: '2016-05-03',
-          name: 'Tom',
-          address: 'No. 189, Grove St, Los Angeles',
-        },
-        {
-          date: '2016-05-02',
-          name: 'Tom',
-          address: 'No. 189, Grove St, Los Angeles',
-        },
-        {
-          date: '2016-05-04',
-          name: 'Tom',
-          address: 'No. 189, Grove St, Los Angeles',
-        },
-        {
-          date: '2016-05-01',
-          name: 'Tom',
-          address: 'No. 189, Grove St, Los Angeles',
-        },
-        {
-          date: '2016-05-03',
-          name: 'Tom',
-          address: 'No. 189, Grove St, Los Angeles',
-        },
-        {
-          date: '2016-05-02',
-          name: 'Tom',
-          address: 'No. 189, Grove St, Los Angeles',
-        },
-        {
-          date: '2016-05-04',
-          name: 'Tom',
-          address: 'No. 189, Grove St, Los Angeles',
-        },
-        {
-          date: '2016-05-01',
-          name: 'Tom',
-          address: 'No. 189, Grove St, Los Angeles',
+          id: 1,
+          title: "学习mybatis",
+          content: "内容"
         }
       ]
+    }
+  },
+  methods: {
+    add() {
+      this.dialogVisible = true
+      // 把表单内容取消
+      this.form = {}
+    },
+    save(){
+      // 请求返回之后，res操作
+      request.post("/blog/add",this.form).then(res => {
+        console.log(res)
+      })
     }
   }
 }
