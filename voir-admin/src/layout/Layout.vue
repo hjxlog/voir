f2ea6ac8cba2d41950f4039eca028b77db284fb3
Huang JX <hjxlog@gmail.com>
2022-03-22 08:52:04 +0800

    update

---
+++ voir-admin/src/layout/Layout.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="common-layout">
+    <el-container>
+      <el-aside width="200px">
+        <BaseAside/>
+      </el-aside>
+      <el-container>
+        <el-header style="padding: 0">
+          <BaseHeader/>
+        </el-header>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+<script>
+import BaseHeader from "@/components/BaseHeader";
+import BaseAside from "@/components/BaseAside";
+
+export default {
+  name: "Layout",
+  components: {
+    BaseHeader,
+    BaseAside
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
